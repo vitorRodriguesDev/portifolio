293429ba0019acf70735bbb6e0a6568f4a9fb26d
vitorRodriguesDev <v9814213@gmail.com>
2024-03-11 21:52:18 -0300

    update-name

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -22,7 +22,7 @@ const HeroSection = () => {
             <br></br>
             <TypeAnimation
               sequence={[
-                "Judy",
+                "Vitor Rodrigues",
                 1000,
                 "Web Developer",
                 1000,
